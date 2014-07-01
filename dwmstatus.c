3b54155fcb4e47859a0b6081bf84053d1cba32c2
dan soucy <dev@danso.ca>
2014-07-01 01:32:59 -0400

    many changes, daemonized, overall smarter

--- dwmstatus.c
+++ dwmstatus.c
@@ -4,6 +4,9 @@
  * pass the argument -Dverbosity=2 when compiling for output
  * or -D experimental_alarm to see what happens (nothing useful)
  * last updated aug 16 2013
+ * tried to add daemon() on may 31 2014
+ * but now it doesn't even compile? all the Xlib stuff is borked
+ * also, replace getavgs() with a read from /proc/loadavg
  */
 
 // this makes gcc not complain about implicit declarations of popen and pclose
@@ -21,7 +24,7 @@
 // const char outformat[] = "[batt: %d%%] [mail %d] [pkg %d] [net %s] %s";
 #define OUTFORMAT "[%.2f %.2f %.2f] [batt: %d%%] [mail %d] [pkg %d] [net %s] %s"
 // level to warn on low battery
-#define WARN_LOW_BATT 12
+#define WARN_LOW_BATT 9
 // files -- populated by cron
 #define MAILFILE "/tmp/dwm-status.mail"
 #define PKGFILE "/tmp/dwm-status.packages"
@@ -32,7 +35,7 @@
 #define zenity
 
 // NDEBUG turns off all assert() calls
-#define NDEBUG
+// #define NDEBUG
 
 #ifndef verbosity
 #define verbosity 0
@@ -137,9 +140,18 @@ static int getbattery(void) {
         fclose(fd);
         if ( ! strncmp(status, "Discharging", 11) ) {
             fprintf(stderr, "low battery warning\n");
-            system("zenity --warning \
-                    --text=\"charge my fuckin' battery!\""); 
-        } 
+            if (fork()) { 
+                // rewrite this with execve
+                // now improve it
+//                 char * gayargs[] = {"zenity", "--warning", 
+//                                     "--text=\"charge my fuckin' battery!\""};
+//                 char * gayenv[] = {NULL};
+//                 execve("/usr/bin/zenity", gayargs, gayenv);
+                system("zenity --warning \
+                        --text=\"charge my fuckin' battery!\"");
+                exit(0);
+            }
+        }
 #endif
     }
 #if verbosity > 1
@@ -175,10 +187,45 @@ static char *net(void) {
     return output;
 }
 
-int main(void) {
+double * getavgs(void) {
+    // THIS is PRETTy DUMB! 
+    // you could just read from /proc/loadavg instead...
+    // must return exactly 3 doubles 
+    double * avgs = malloc(3 * sizeof(double));
+    int num_avgs = getloadavg(avgs, 3);
+    if (num_avgs < 3) {
+        if (num_avgs == -1) {
+            fprintf(stderr, "num_avgs is -1");
+            num_avgs = 0;
+        } 
+        for (int i = num_avgs; i < 3; i++) {
+            avgs[i] = 9.9;
+        }
+    }
+    return avgs;
+}
+
+/*
+char * getavgs(void) {
     double avgs[3];
+    if (getloadavg(avgs, 3) < 0) {
+        perror("getloadavg broke");
+        // return "oops";
+        exit(1);
+    }
+    return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+}
+*/
+
+int main(void) {
+    daemon(0, 0);
+    // int num_avgs;
+    // double avgs[3];
     static Display *dpy;
     char *status;
+
+    double * avgs;
+    // printf("THE NUMBER: %f\n", avgs[0]);
 
     if (!(dpy = XOpenDisplay(NULL))) {
         fprintf(stderr, "Cannot open display.\n");
@@ -190,17 +237,7 @@ int main(void) {
     }
     
     for ( ; ; sleep(59)) {
-        // get load averages
-        int num_avgs = getloadavg(avgs, 3);
-        if (num_avgs < 3) {
-            if (num_avgs == -1) {
-                fprintf(stderr, "num_avgs is -1");
-                num_avgs = 0;
-            } 
-            for (int i = num_avgs; i < 3; i++) {
-                avgs[i] = 9.9;
-            }
-        }
+        avgs = getavgs();
 
         snprintf(status, 90, OUTFORMAT, 
                  // getfiledata("/tmp/dwm-status.mail"), 
