4d19718a3d309ab53de215ec8f134e2c1b361b45
dan soucy <dev@danso.ca>
2014-08-04 16:35:18 -0400

    major overhaul: commandline args, new net usage functionality

--- dwmstatus.c
+++ dwmstatus.c
@@ -3,15 +3,19 @@
  * dan: compile with: gcc -Wall -pedantic -lX11 -std=c99 dwm-status.c
  * pass the argument -Dverbosity=2 when compiling for output
  * or -D experimental_alarm to see what happens (nothing useful)
- * last updated aug 16 2013
+ * last updated aug 04 2014
  * also, replace getavgs() with a read from /proc/loadavg
+ * make WARN_LOW_BATT_TEXT do something, the ""s make it tricky
+ * add cmdline args to either print immediately or daemon
+ *     make verboseMode do something or get rid of it
  */
 
 // this makes gcc not complain about implicit declarations of popen and pclose
 // when using -pedantic and -std=c99. i guess that's good?
 // also provides getloadavg
 // see: man 7 feature_test_macros
-#define _BSD_SOURCE
+// #define _BSD_SOURCE
+#define _GNU_SOURCE
 
 /* SETTINGS */
 
@@ -20,9 +24,11 @@
 #define TIMESTRING "[%a %b %d] %H:%M"
 // the format for everything
 // const char outformat[] = "[batt: %d%%] [mail %d] [pkg %d] [net %s] %s";
-#define OUTFORMAT "[%.2f %.2f %.2f] [batt: %d%%] [mail %d] [pkg %d] [net %s] %s"
+// #define OUTFORMAT "[%.2f %.2f %.2f] [batt: %d%%] [mail %d] [pkg %d] [net %s] %s"
+#define OUTFORMAT "[%.2f %.2f %.2f] [%s] [batt: %d%%] [mail %d] [net %s] %s"
 // level to warn on low battery
 #define WARN_LOW_BATT 9
+#define WARN_LOW_BATT_TEXT "you're a fat slut"
 // files -- populated by cron
 #define MAILFILE "/tmp/dwm-status.mail"
 #define PKGFILE "/tmp/dwm-status.packages"
@@ -33,7 +39,7 @@
 #define zenity
 
 // NDEBUG turns off all assert() calls
-#define NDEBUG
+// #define NDEBUG
 
 #ifndef verbosity
 #define verbosity 0
@@ -46,7 +52,12 @@
 #include <string.h>
 #include <assert.h>
 #include <X11/Xlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include "dwm-status.h"
 // #include <netdb.h>
+
+const char * program_name;
 
 static void setstatus(const char *str, Display *dpy) {
     assert(dpy != NULL);
@@ -140,9 +151,9 @@ static int getbattery(void) {
         if ( ! strncmp(status, "Discharging", 11) ) {
             fprintf(stderr, "low battery warning\n");
             if (fork()) { 
-                char * const gayargs[] = {"zenity", "--warning", "--text=you're a fat slut", NULL};
+                char * const args[] = {"zenity", "--warning", "--text=you're a fat slut", NULL};
 //                 execv("zenity", {"--warning", "--text=what", NULL});
-                execv("/usr/bin/zenity", gayargs);
+                execv("/usr/bin/zenity", args);
                     /*
                 system("zenity --warning \
                         --text=\"charge my fuckin' battery!\"");
@@ -175,14 +186,26 @@ static char *net(void) {
     fgets(output, 4, fp);
     pclose(fp);
 
+    char * result = malloc(40);
+    snprintf(result, 15, "\x1b[38;5;196m");
+    
+    strcat(result, output);
+
     // the last character might be an unwanted newline
+    /*
     if (output[strlen(output)-1] == '\n') {
         output[strlen(output)-1] = '\0';
     }
+    */
+    if (result[strlen(result)-1] == '\n') {
+        result[strlen(result)-1] = '\0';
+    }
+    strcat(result, "\x1b[0m");
 #if verbosity > 1
     printf("returning %s from net()\n", output);
 #endif
-    return output;
+//     return output;
+    return result;
 }
 
 double * getavgs(void) {
@@ -215,43 +238,236 @@ char * getavgs(void) {
 }
 */
 
-int main(void) {
-    daemon(0, 0);
-    // int num_avgs;
-    // double avgs[3];
+void usage(FILE * stream, int exit_code) {
+    // prints what's up to stream, then quits with status exit_code
+    fprintf(stream, "usage: %s [args]\n", program_name);
+    fprintf(stream, " -h --help         display this message\n");
+    fprintf(stream, " -v --verbose      verbose mode, does nothing LOL\n");
+    fprintf(stream, " -d --daemon       run in background\n");
+    fprintf(stream, " -r --report       report status immediately (default)\n");
+    exit(exit_code);
+}
+
+
+
+ // obviously do this right
+char * getColour(void) {
+    char * colour = malloc(100);
+    snprintf(colour, 100, "\x1b[38;5;196m"); // red
+    return colour;
+}
+
+           
+
+
+int main(int argc, char * argv[]) {
+
+    program_name = argv[0];
+    
+    bool verboseMode = false;
+    /* list available args */
+    const char * const shortOptions = "hvdr";
+    const struct option longOptions[] = {
+        {"help", 0, NULL, 'h'},
+        // if verboseMode was an int, you could do this:
+        // this means if --verbose is in the args, set verboseMode to 1
+//         {"verbose", 0, &verboseMode, 1}, 
+        {"verbose", 0, NULL, 'v'}, 
+        {"daemon", 0, NULL, 'd'},
+        {"report", 0, NULL, 'r'},
+    };
+
+    /* parse args */
+    int nextOption;
+    bool daemonMode = false;
+
+    do {
+        nextOption = getopt_long(argc, argv, shortOptions, longOptions, NULL);
+        switch (nextOption) {
+            case 'h':
+                usage(stdout, 0);
+            case 'v':
+                printf("goin into verbose mode LOL NOT REALLY\n");
+                verboseMode = true;
+                break;
+            case 'd':
+//                 printf("goin into daemon mode\n");
+                daemonMode = true;
+                break;
+            case 'r':
+//                 printf("goin into default mode\n");
+                daemonMode = false;
+                break;
+            case '?':
+                usage(stderr, 1);
+            case (-1): 
+                break;
+            default:
+                // when does this happen? 
+                printf("what? exiting\n");
+                exit(100);
+        }
+    } while (nextOption != -1);
+
     static Display *dpy;
-    char *status;
-
-    double * avgs;
-    // printf("THE NUMBER: %f\n", avgs[0]);
-
     if (!(dpy = XOpenDisplay(NULL))) {
         fprintf(stderr, "Cannot open display.\n");
         return EXIT_FAILURE;
     }
-    if ((status = malloc(80)) == NULL) {
-        fprintf(stderr, "status = malloc(80) failed.\n");
-        return EXIT_FAILURE;
-    }
-    
-    for ( ; ; sleep(59)) {
-        avgs = getavgs();
 
-        snprintf(status, 90, OUTFORMAT, 
-                 // getfiledata("/tmp/dwm-status.mail"), 
-                 avgs[0], avgs[1], avgs[2],
-                 getbattery(), 
-                 getfiledata(MAILFILE), 
-                 getfiledata(PKGFILE), 
-                 // getfiledata("/tmp/dwm-status.packages"),
-                 net(), getdatetime());
-#if verbosity > 0
-        puts(status);
-#endif
-        setstatus(status, dpy);
-    }
+    if (daemonMode) {
+        return daemonize(dpy);
+    } 
 
-    free(status);
-    XCloseDisplay(dpy);
+    updateStatus(dpy);
+    if (verboseMode) { // this is currently the only place verboseMode is used
+        printf("now quittin!\n"); // LOL
+    }
     return 0;
+}
+
+void updateStatus(Display *dpy) {
+//     printf("%s\n", getColour() + 1);
+	static unsigned long long int recv, sent;
+    static char * status;
+    double * avgs = getavgs();
+    // calling parse_netdev() first will initialize recv and sent to
+    // useful values, but if i do this here, parse_netdev() will be called
+    // every time updateStatus() is called, resulting in bad values
+// 	parse_netdev(&recv, &sent);
+
+    // thank you, _GNU_SOURCE, for asprintf
+    // asprintf returns -1 on error, we check for that 
+
+    if (asprintf(&status, OUTFORMAT,
+             avgs[0], avgs[1], avgs[2],
+             get_netusage(&recv, &sent),
+             getbattery(),
+             getfiledata(MAILFILE), 
+//                  getfiledata(PKGFILE), 
+             net(), getdatetime()) == -1) {
+        fprintf(stderr, "error, unable to malloc() in asprintf()");
+        exit(1);
+    }
+
+    setstatus(status, dpy);
+    free(status);
+    // obviously do this right
+//     setstatus(strcat(getColour(), status), dpy);
+
+    /*
+    if (verboseMode) {
+        printf("%s\n", status);
+    }
+    */
+}
+
+
+int daemonize(Display * dpy) {
+//     printf("running daemon HEHE\n");
+    daemon(0, 0);
+  
+// 	static unsigned long long int recv, sent;
+//     double * avgs;
+// 	parse_netdev(&recv, &sent);
+    for (; ; sleep(1)) {
+    /*
+    for (int x = 0; ; sleep(1)) {
+        printf("iteration %i!\n", x);
+        x++;
+        */
+        updateStatus(dpy);
+    }
+    return 0;
+}
+
+/* --------------------------------------------------  
+   net stuff begins here, with thanks to anonymous suckless contributor
+   */
+
+bool parse_netdev(unsigned long long int *receivedabs, 
+                  unsigned long long int *sentabs) {
+	char buf[255];
+	char *datastart;
+	static int bufsize;
+	bool success;
+	FILE *devfd;
+	unsigned long long int receivedacc, sentacc;
+
+	bufsize = 255;
+	devfd = fopen("/proc/net/dev", "r");
+	success = false;
+
+	// Ignore the first two lines of the file
+	fgets(buf, bufsize, devfd);
+	fgets(buf, bufsize, devfd);
+
+	while (fgets(buf, bufsize, devfd)) {
+	    if ((datastart = strstr(buf, "lo:")) == NULL) {
+            datastart = strstr(buf, ":");
+
+            // With thanks to the conky project at http://conky.sourceforge.net/
+            sscanf(datastart + 1, "%llu  %*d     %*d  %*d  %*d  %*d   %*d        %*d       %llu",\
+                   &receivedacc, &sentacc);
+            *receivedabs += receivedacc;
+            *sentabs += sentacc;
+            success = true;
+	    }
+	}
+
+	fclose(devfd);
+	return success;
+}
+
+void calculate_speed(char *speedstr, unsigned long long int newval, 
+                     unsigned long long int oldval) {
+	double speed;
+    char unit;
+	speed = (newval - oldval) / 1024.0;
+	if (speed > 1024.0) {
+	    speed /= 1024.0;
+        unit = 'm';
+// 	    sprintf(speedstr, "%.3f MB/s", speed);
+// 	    sprintf(speedstr, "%.2fm", speed);
+	} else {
+        unit = 'k';
+// 	    sprintf(speedstr, "%.2f KB/s", speed);
+// 	    sprintf(speedstr, "%.2fk", speed);
+	}
+    // pad the beginning of the string so it uses the same space for all sizes
+    char * padding;
+    if (speed < 10) {
+        padding = "  ";
+    } else if (speed < 100) {
+        padding = " ";
+    } else {
+        padding = "";
+    }
+    sprintf(speedstr, "%s%.2f%c", padding, speed, unit);
+
+}
+
+char * get_netusage(unsigned long long int *recv, 
+                    unsigned long long int *sent) {
+    // this function has the side effect of updating the values of recv and sent
+	unsigned long long int newrecv, newsent;
+	newrecv = newsent = 0;
+
+	bool retval = parse_netdev(&newrecv, &newsent);
+	if (! retval) {
+	    fprintf(stdout, "Error when parsing /proc/net/dev file.\n");
+	    exit(1);
+	}
+
+	char downspeedstr[15], upspeedstr[15];
+	calculate_speed(downspeedstr, newrecv, *recv);
+	calculate_speed(upspeedstr, newsent, *sent);
+
+	static char retstr[42];
+// 	sprintf(retstr, "down: %s up: %s", downspeedstr, upspeedstr);
+	sprintf(retstr, "V:%s Λ:%s", downspeedstr, upspeedstr);
+
+	*recv = newrecv;
+	*sent = newsent;
+	return retstr;
 }
