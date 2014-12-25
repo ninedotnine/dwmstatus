c14b88a46bf478f5c8a730441d2a3b2c1665e84e
dan soucy <dev@danso.ca>
2014-12-25 05:14:09 -0500

    Update dwm-status.h

--- dwm-status.h
+++ dwm-status.h
@@ -1,19 +1,11 @@
-static void setstatus(const char *str, Display *dpy);
-static char *getdatetime(void); 
-static int getfiledata(const char *filename); 
-static int getbattery(void); 
-static char *net(void); 
-double * getavgs(void); 
+char *getdatetime(void); 
+int getfiledata(const char *filename); 
+char * getTemperature(void);
+char * getBattery(void); 
+char *net(void); 
+double * getAvgs(void); 
 void usage(FILE * stream, int exit_code); 
-int daemonize(Display *dpy);
-void updateStatus(Display * dpy);
-
-// net stuff begins here
-
-bool parse_netdev(unsigned long long int *receivedabs, 
-                  unsigned long long int *sentabs);
-void calculate_speed(char *speedstr, unsigned long long int newval, 
-                     unsigned long long int oldval);
-char * get_netusage(unsigned long long int *rec, unsigned long long int *sent);
-
-
+char * getColour(int);
+char * resetColour(void);
+void setStatus(Display *dpy);
+char * buildStatus(void);
