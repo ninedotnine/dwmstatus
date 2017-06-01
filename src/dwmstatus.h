a327958b3a0812e8836abeb97e12ced0c740aa7f
dan soucy <dev@danso.ca>
2017-06-01 02:18:09 -0400

    correct function declaration

--- src/dwmstatus.h
+++ src/dwmstatus.h
@@ -5,5 +5,5 @@ void getBattery(char * (* const batt));
 void net(char * (* const netOK));
 void getAvgs(double (* avgs2)[3]);
 void usage(FILE * stream, int exit_code);
-void setStatus();
+void setStatus(void);
 char * buildStatus(void);
